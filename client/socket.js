93a58e9da2a095015ddf681469b19bae2dbefc38
Manuel Bugallo <marvinody@gmail.com>
2020-01-24 03:39:26 -0500

    feat: adds rudimentary socket error handling

--- client/socket.js
+++ client/socket.js
@@ -1,9 +1,17 @@
 import io from 'socket.io-client'
+import {ACTIONS, EVENT} from '../constants/socket'
+import history from './history'
 
 const socket = io(window.location.origin)
 
 socket.on('connect', () => {
   console.log('Connected!')
+})
+
+socket.on(EVENT.ERROR, ({redirect, message}) => {
+  console.error(message)
+  alert(message)
+  history.push(redirect)
 })
 
 export default socket
